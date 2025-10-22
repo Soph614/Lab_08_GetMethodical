9173ab04bb79a2c58c7fbce2b395145cc992ea4c
soph <sophia@javamate.net>
2025-10-22 19:07:40 -0400

    Commit 4

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -4,16 +4,15 @@ public class CheckOut {
     public static void main(String[] args) {
         Scanner pipe = new Scanner(System.in);
         boolean retVal = false;
-        double price = 0;
-        double userDouble = 0;
+        double price = 0.0;
+        double userDouble = 0.0;
         do {
             userDouble = SafeInput.getRangedDouble(pipe, "Enter the price of your item", .50, 10.00);
             price = userDouble + price;
-            pipe.nextLine();
+            // pipe.nextLine();
             retVal = DevTest.getYNConfirm(pipe, "If you're done, press 'Y'; if not, press 'N'");
         } while(!retVal);
-        String totalPriceDisplay = "TOTAL PRICE:";
-        System.out.printf("%-14s%5s", totalPriceDisplay, price);
+        System.out.printf("TOTAL PRICE: %5s", price);
 
     }
 }
